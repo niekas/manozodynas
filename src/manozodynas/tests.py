d98dd249d01d7cbb45006208184e251b405c37e3
Gezzas <tgasionis@gmail.com>
2014-05-12 19:31:09 +0300

    Added test for Login view.

--- src/manozodynas/tests.py
+++ src/manozodynas/tests.py
@@ -8,3 +8,61 @@ class IndexTestCase(StatefulTesting):
     def test_index_page(self):
         self.open(reverse('index'))
         self.assertStatusCode(200)
+
+
+class LoginTestCase(StatefulTesting):
+
+    fixtures = ['test_fixture.json']
+
+    def test_login_page(self):
+        self.open(reverse('login'))
+        self.assertStatusCode(200)
+
+    def test_good_login(self):
+        self.open(reverse('login'))
+        self.selectForm('#login')
+        self.submitForm({
+            'username': 'test',
+            'password': 'test',
+        })
+        self.assertStatusCode(302)
+
+    def test_bad_login(self):
+        self.open(reverse('login'))
+        self.selectForm('#login')
+        self.submitForm({
+            'username': 'bad',
+            'password': 'bad',
+        })
+        self.assertStatusCode(200)
+        self.selectOne('.errorlist')
+
+    def test_no_input(self):
+        self.open(reverse('login'))
+        self.selectForm('#login')
+        self.submitForm({
+            'username': '',
+            'password': '',
+        })
+        self.assertStatusCode(200)
+        self.selectMany('.errorlist')
+
+    def test_no_username(self):
+        self.open(reverse('login'))
+        self.selectForm('#login')
+        self.submitForm({
+            'username': '',
+            'password': 'test',
+        })
+        self.assertStatusCode(200)
+        self.selectOne('.errorlist')
+
+    def test_no_password(self):
+        self.open(reverse('login'))
+        self.selectForm('#login')
+        self.submitForm({
+            'username': 'test',
+            'password': '',
+        })
+        self.assertStatusCode(200)
+        self.selectOne('.errorlist')
